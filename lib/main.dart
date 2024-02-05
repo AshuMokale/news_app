d5d6869ff93b3a0b86b98d1ffd8c0e801448c315
Rahul Roy <rahul@gmail.com>
2024-02-05 09:05:12 +0530

    new login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,14 @@
+
+
 import 'package:flutter/material.dart';
+import 'package:news_app/login.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import '';
+void main(){
+  runApp(MaterialApp(
+   initialRoute: 'login',
+    routes: { 'login': (context)=> MyLogin()},
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: LoginPage(),
-    );
-  }
-}
 
-class LoginPage extends StatefulWidget {
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  TextEditingController _usernameController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Page'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.network(
-              'http:\Users\RAHUL\AndroidStudioProjects\news_app\assets\img21.jpg', // Replace with your actual image URL
-              height: 100.0,
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              controller: _usernameController,
-              decoration: InputDecoration(
-                labelText: 'Username',
-              ),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
-            SizedBox(height: 32.0),
-            ElevatedButton(
-              onPressed: () {
-                // Add your login logic here
-                // You can check the credentials and navigate to the news page if successful
-                // For simplicity, I'll just print the entered credentials
-                print('Username: ${_usernameController.text}');
-                print('Password: ${_passwordController.text}');
-              },
-              child: Text('Login'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  ));
 }
