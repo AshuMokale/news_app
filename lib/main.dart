5059969e295cbb10a83cf78cc613bf5d390955d9
VaishnavRoy <rahulvaishnav2402@gmail.com>
2024-02-05 21:27:13 +0530

    features added to login page

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,12 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/login.dart';
 
-import '';
+
 void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
    initialRoute: 'login',
-    routes: { 'login': (context)=> MyLogin()},
+    routes: { 'login': (context)=> const MyLogin()},
 
 
   ));
