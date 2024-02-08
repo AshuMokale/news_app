f346c269579690837f63f1887567665336b32b19
Rahul Roy <rahul@gmail.com>
2024-02-08 14:06:00 +0530

    Preparing to merge.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:news_app/Register.dart';
 import 'package:news_app/login.dart';
@@ -15,51 +13,5 @@ void main(){
     },
 
 
-//<<<<<<< HEAD
   ));
 }
-=======
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Screen'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _usernameController,
-              decoration: InputDecoration(
-                labelText: 'Username',
-              ),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
-              ),
-            ),
-            SizedBox(height: 24.0),
-            ElevatedButton(
-              onPressed: () {
-                // Implement your authentication logic here
-                String username = _usernameController.text;
-                String password = _passwordController.text;
-
-                // For simplicity, we are just printing the entered credentials
-                print('Username: $username, Password: $password');
-              },
-              child: Text('login'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
->>>>>>> a2beed23053d176a351afbf9a78f9ac7bcdb8ec4
