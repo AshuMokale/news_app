66273111dbf71d68081bd5c3212627baed5aa786
Ashish Mokale <ashishmokale2002@gmail.com>
2024-05-25 09:39:48 +0530

    Removed favourite articles button.

--- lib/api/article_screen.dart
+++ lib/api/article_screen.dart
@@ -4,41 +4,91 @@ import 'package:flutter/material.dart';
 import 'package:news_app/api/articles.dart';
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' as htmlParser;
-// import 'package:html/dom.dart' as htmlDom;
 
-class ArticleDetailScreen extends StatelessWidget {
-  ArticleDetailScreen({required this.article, super.key});
+class ArticleDetailScreen extends StatefulWidget {
+  ArticleDetailScreen({required this.article, Key? key}) : super(key: key);
   final Article article;
 
+  @override
+  State<ArticleDetailScreen> createState() => _ArticleDetailScreenState();
+}
+
+class _ArticleDetailScreenState extends State<ArticleDetailScreen> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  bool isFavorite = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkIfFavorite();
+  }
 
   Future<void> _addToFavorites(BuildContext context) async {
     User? user = _auth.currentUser;
     if (user != null) {
       try {
-        await _firestore.collection('favourite_articles').add({
-          'uid': user.uid,
-          'author': article.author,
-          'title': article.title,
-          'description': article.description,
-          'url': article.url,
-          'urlToImage': article.urlToImage,
-          'publishedAt': article.publishedAt,
-          'content': article.content,
-        });
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Added to Favorites')),
-        );
+        // Use doc() to get a reference to the document with the article URL
+        final documentReference =
+            _firestore.collection('favourite_articles').doc(widget.article.url);
+
+        // Check if the document exists before deleting it
+        final snapshot = await documentReference.get();
+        if (snapshot.exists) {
+          // Document exists, do not delete it, instead, show feedback to the user
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Article already in Favorites')),
+          );
+        } else {
+          // Document doesn't exist, add it to favorites
+          await documentReference.set({
+            'uid': user.uid,
+            'author': widget.article.author,
+            'title': widget.article.title,
+            'description': widget.article.description,
+            'url': widget.article.url,
+            'urlToImage': widget.article.urlToImage,
+            'publishedAt': widget.article.publishedAt,
+            'content': widget.article.content,
+          });
+          setState(() {
+            isFavorite = true;
+          });
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Added to Favorites')),
+          );
+        }
       } catch (e) {
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Failed to add to favorites: $e')),
+          SnackBar(content: Text('Failed to manage favorites: $e')),
         );
       }
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Please log in to add favorites')),
+        SnackBar(content: Text('Please log in to manage favorites')),
       );
+    }
+  }
+
+  void _toggleFavorites(BuildContext context) {
+    if (isFavorite) {
+      _addToFavorites(context);
+    } else {
+      _addToFavoritesFromApi();
+    }
+  }
+
+  Future<void> _checkIfFavorite() async {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      QuerySnapshot snapshot = await _firestore
+          .collection('favourite_articles')
+          .where('uid', isEqualTo: user.uid)
+          .where('url', isEqualTo: widget.article.url)
+          .get();
+      setState(() {
+        isFavorite = snapshot.docs.isNotEmpty;
+      });
     }
   }
 
@@ -66,7 +116,7 @@ class ArticleDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(article.title),
+        title: Text(widget.article.title),
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
@@ -74,7 +124,7 @@ class ArticleDetailScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              article.title,
+              widget.article.title,
               style: const TextStyle(
                 fontSize: 24.0,
                 fontWeight: FontWeight.bold,
@@ -82,19 +132,15 @@ class ArticleDetailScreen extends StatelessWidget {
             ),
             const SizedBox(height: 8.0),
             Text(
-              'By ${article.author}',
+              'By ${widget.article.author}',
               style: const TextStyle(
                 fontStyle: FontStyle.italic,
               ),
             ),
             const SizedBox(height: 16.0),
-            // Text(
-            //   article.content,
-            //   style: const TextStyle(fontSize: 16.0),
-            // ),
             Center(
               child: FutureBuilder<String>(
-                future: parseHtmlFromUrl(article.url),
+                future: parseHtmlFromUrl(widget.article.url),
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
                     return Text(snapshot.data ?? '');
@@ -108,17 +154,55 @@ class ArticleDetailScreen extends StatelessWidget {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        // onPressed: () {
-        //   ScaffoldMessenger.of(context).showSnackBar(
-        //     const SnackBar(
-        //       content: Text('Added to favorites!'),
-        //     ),
-        //   );
-        // },
-        onPressed: () => _addToFavorites(context),
-        child: const Icon(Icons.favorite),
-      ),
+      floatingActionButton: isFavorite
+          ? null
+          : FloatingActionButton(
+              onPressed: () => _toggleFavorites(context),
+              child: Icon(Icons.favorite_border),
+            ),
     );
+  }
+
+// void _toggleFavorites(BuildContext context) {
+//   if (isFavorite) {
+//     _addToFavorites(context);
+//   } else {
+//     _addToFavoritesFromApi();
+//   }
+// }
+
+  Future<void> _addToFavoritesFromApi() async {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      try {
+        // Use doc() to generate a unique document ID
+        final documentReference =
+            _firestore.collection('favourite_articles').doc();
+        await documentReference.set({
+          'uid': user.uid,
+          'author': widget.article.author,
+          'title': widget.article.title,
+          'description': widget.article.description,
+          'url': widget.article.url,
+          'urlToImage': widget.article.urlToImage,
+          'publishedAt': widget.article.publishedAt,
+          'content': widget.article.content,
+        });
+        setState(() {
+          isFavorite = true;
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Added to Favorites')),
+        );
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to add to favorites: $e')),
+        );
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Please log in to add favorites')),
+      );
+    }
   }
 }
